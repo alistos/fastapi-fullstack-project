bf1b2e8c3ad9a99b49eb3d13735a1830af8969d2
Alisson dos Santos Silva <alss@NEUROTECH.INTRANET>
2022-03-11 14:28:20 -0300

    Consertando bug no arquivo database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
-if SQLALCHEMY_DATABASE_URL.startswith("postgres://)"):
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(
