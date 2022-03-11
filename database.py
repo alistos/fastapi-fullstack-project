05146abacfb0f4f232cfaa7f9f4c6a49b6f107fc
Alisson dos Santos Silva <alss@NEUROTECH.INTRANET>
2022-03-11 14:01:45 -0300

    Setando a aplicação para rodar com database postgres no heroku

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:mypass14@127.0.0.1:3306/todoapp"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://)"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
